1e2b4ae1755510b5ead1971ba722450fc6652458
YerkoPalma <yerko.palma@usach.cl>
2019-04-22 01:51:07 +0000

    updated fs-router

--- components/fs-router.js
+++ components/fs-router.js
@@ -1,11 +1,14 @@
 /* global fetch customElements */
 import { LitElement, html } from 'https://unpkg.com/@polymer/lit-element?module'
-import { md } from 'https://unpkg.com/md?module'
+import { md } from '../lib/markdown.js'
 
 export default class FsRouter extends LitElement {
   static get properties () {
     return {
       defaultPage: {
+        type: String
+      },
+      startPage: {
         type: String
       },
       content: {
@@ -15,34 +18,59 @@ export default class FsRouter extends LitElement {
   }
   constructor () {
     super()
+    this.startPage = window.location.href
+    this.content = html([md(this.innerHTML)])
+    this.initialContent = this.content
     window.addEventListener('popstate', e => {
       e.preventDefault()
       // fetch routes
-      this.resolve().then(result => { this.content = result })
+      this.resolve().then(result => {
+        this.content = result
+      })
     })
   }
-  async resolve () {
-    const isHtml = /\.html?$/i.test(window.location.pathname)
-    const isMd = /\.(?:md|markdown)$/i.test(window.location.pathname)
+  firstUpdated (changedProperties) {
+    // fetch default content
+    for (let attribute of Array.from(this.attributes)) {
+      if (attribute.name === 'default-page') {
+        this.resolve(attribute.value).then(result => { this.defaultContent = result })
+      }
+    }
+    let links = document.querySelectorAll('a[href]')
+    Array.prototype.forEach.call(links, link => {
+      link.addEventListener('click', ev => {
+        ev.preventDefault()
+        this.resolve(link.href).then(result => {
+          this.content = result
+          window.history.pushState(undefined, link.href, link.href)
+        })
+      })
+    })
+  }
+  async resolve (path) {
+    path = path || window.location.href
+    if (path === this.startPage) return this.initialContent
+    const isHtml = /\.html?$/i.test(path)
+    const isMd = /\.(?:md|markdown)$/i.test(path)
     const maybeFolder = !isHtml && !isMd
     let response
     let result
     try {
-      response = await fetch(window.location.pathname)
+      response = await fetch(path)
       if (response && response.ok) {
         result = await response.text()
-        if (isHtml) return html([result])
         if (isMd) return html([md(result)])
+        else return html([result])
       } else if (maybeFolder) {
         // maybe we need to add the extension or add an /index.(html|md) path
         // try adding extension first
-        response = await fetch(`${window.location.pathname}.html`)
+        response = await fetch(`${path}.html`)
         if (response && response.ok) {
           // we found an html file
           result = await response.text()
           return html([result])
         } else {
-          response = await fetch(`${window.location.pathname}.md`)
+          response = await fetch(`${path}.md`)
           if (response && response.ok) {
             // we found an md file
             result = await response.text()
@@ -50,14 +78,14 @@ export default class FsRouter extends LitElement {
           } else {
             // if we are here then we need to add an index file to the path
             // first try html
-            response = await fetch(`${window.location.pathname}/index.html`)
+            response = await fetch(`${path}/index.html`)
             // we found index.html
             if (response && response.ok) {
               result = await response.text()
               return html([result])
             } else {
               // last chance, go for index.md
-              response = await fetch(`${window.location.pathname}/index.md`)
+              response = await fetch(`${path}/index.md`)
               if (response && response.ok) {
                 result = await response.text()
                 return html([md(result)])
@@ -68,6 +96,8 @@ export default class FsRouter extends LitElement {
       }
     } catch (e) {
     }
+    // if we are here, return default content
+    return this.defaultContent
   }
   render () {
     return this.content
