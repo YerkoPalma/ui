7eabbfe1aa2200f118ed4185451c13e25b088145
Yerko Palma <yerko.palma@usach.cl>
2019-04-27 19:31:12 -0400

    fix routes and add 404.html and 200.html

--- scripts/publish.js
+++ scripts/publish.js
@@ -7,6 +7,10 @@ const pkg = require('../package.json')
 fs.writeFileSync(path.join(__dirname, '..', 'docs', 'CNAME'), pkg.homepage)
 fs.writeFileSync(path.join(__dirname, '..', 'docs', '.nojekyll'), '')
 
+// copy index as 404.html and 200.html
+fs.copyFileSync(path.join(__dirname, '..', 'docs', 'index.html'), path.join(__dirname, '..', 'docs', '404.html'))
+fs.copyFileSync(path.join(__dirname, '..', 'docs', 'index.html'), path.join(__dirname, '..', 'docs', '200.html'))
+
 publish('docs', {
   dotfiles: true
 }, console.log)
