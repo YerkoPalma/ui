553b351b5b25f9f72f43c86d54b1453cc8bb00d3
Yerko Palma <yerko.palma@usach.cl>
2019-04-27 18:10:46 -0400

    reorder files and update build flow

--- scripts/build.js
+++ scripts/build.js
@@ -1,9 +1,27 @@
 const fs = require('fs')
+const path = require('path')
 const browserify = require('browserify')
 const babelify = require('babelify')
 
-browserify('./index.js')
-  .transform(babelify.configure({ presets: [ '@babel/preset-env' ], plugins: [ '@babel/transform-runtime' ] }), { global: true })
-  .plugin('tinyify', { flat: false })
-  .bundle()
-  .pipe(fs.createWriteStream('bundle.js'))
+function makeBundle (file) {
+  const input = path.join(__dirname, '..', 'src', file)
+  const output = path.join(__dirname, '..', 'dist', file)
+
+  fs.mkdir(path.dirname(input), { recursive: true }, err => {
+    if (err) console.log(err)
+    fs.mkdir(path.dirname(output), { recursive: true }, err => {
+      if (err) console.log(err)
+      browserify(input)
+        .transform(babelify.configure({ presets: [ '@babel/preset-env' ], plugins: [ '@babel/transform-runtime' ] }), { global: true })
+        .plugin('tinyify', { flat: false })
+        .bundle()
+        .pipe(fs.createWriteStream(output))
+    })
+  })
+}
+
+makeBundle('index.js')
+makeBundle('components/app-shell.js')
+makeBundle('components/flipper-card.js')
+makeBundle('components/fs-router.js')
+makeBundle('components/md-view.js')
--- scripts/publish.js
+++ scripts/publish.js
@@ -1,13 +1,12 @@
 const { publish } = require('gh-pages')
 const fs = require('fs')
+const path = require('path')
 const pkg = require('../package.json')
 
 // create CNAME and .nojekyll file
-fs.writeFile('CNAME', pkg.homepage)
-fs.writeFile('.nojekyll')
+fs.writeFileSync(path.join(__dirname, '..', 'docs', 'CNAME'), pkg.homepage)
+fs.writeFileSync(path.join(__dirname, '..', 'docs', '.nojekyll'), '')
 
 publish('docs', {
   dotfiles: true
 }, console.log)
-
-// generate bundle for every component
