3f8132fdb1ba30ce7d9fc0e214ac4cdf3f078df6
Yerko Palma <yerko.palma@usach.cl>
2019-05-13 22:32:26 -0400

    side-nav: fix responsive version

--- src/components/side-nav.js
+++ src/components/side-nav.js
@@ -1,4 +1,4 @@
-/* global customElements fetch */
+/* global customElements fetch HTMLAnchorElement getComputedStyle */
 import { LitElement, html } from 'https://unpkg.com/lit-element?module'
 
 export default class SideNav extends LitElement {
@@ -45,12 +45,41 @@ export default class SideNav extends LitElement {
       }
       ${this.customStyle ? this.customStyle : ''}
     </style>
-    <aside class="z-999 helvetica bg-black-90 top-0 fixed h-100 ph3 pv3 pv4-ns ph3-m ph4-l">
+    <aside class="z-999 helvetica bg-black-90 top-0 fixed h-100 ph3 pv3 pv4-ns ph3-m ph4-l" @click="${this.toggleNav}">
       <nav class="f6 fw6 ttu tracked">
         <slot></slot>
       </nav>
     </aside>
     `
+  }
+  firstUpdated () {
+    let slot = this.shadowRoot.querySelector('slot')
+    let anchors = slot.assignedNodes().filter(el => el instanceof HTMLAnchorElement)
+    anchors.forEach(a => {
+      a.addEventListener('click', e => {
+        e.preventDefault()
+        e.stopPropagation()
+        if (this.isOpen) {
+          this.isOpen = false
+          anchors.forEach(an => {
+            an.style.display = 'none'
+          })
+        }
+      })
+    })
+  }
+  toggleNav (e) {
+    e.preventDefault()
+    e.stopPropagation()
+    let slot = this.shadowRoot.querySelector('slot')
+    let anchors = slot.assignedNodes().filter(el => el instanceof HTMLAnchorElement)
+    // if links are hidden, show them and grow nav
+    if (getComputedStyle(anchors[0]).display === 'none') {
+      this.isOpen = true
+      anchors.forEach(a => {
+        a.style.display = 'block'
+      })
+    }
   }
 }
 
